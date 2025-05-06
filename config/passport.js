42419797803c9228e93cc6ec4b8ccb674ebee9b2
wulandari-tech <zanssxploit@gmail.com>
2025-05-06 09:07:01 +0700

    wanzz

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,6 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const User = require('../models/User'); // Adjust path as needed
-
+const User = require('../models/user'); 
 passport.use(new LocalStrategy(
     async (username, password, done) => {
         try {
